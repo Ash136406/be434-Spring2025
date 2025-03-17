397fa69f09b20ed7408ea0ca5796c31269e61906
Bonnie Hurwitz <bhurwitz@arizona.edu>
2025-03-17 11:53:19 -0700

    fixing to add a file option

--- assignments/04_revc/revc.py
+++ assignments/04_revc/revc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import argparse
-
+import os
 
 # --------------------------------------------------
 def get_args():
@@ -11,26 +11,34 @@ def get_args():
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('sequence',
-                        metavar='str',
-                        help='DNA sequence')
+                        metavar='DNA',
+                        help='DNA sequence or input file')
 
-    parser.add_argument('-f',
-                        '--file',
-                        nargs='?',
-                        help='File containing the DNA sequence')
+#    parser.add_argument('-f',
+#                        '--file',
+#                        nargs='?',
+#                        help='File containing the DNA sequence')
 
-    return parser.parse_args()
+# you can check to see if sequence is a file or string like so...
+    args = parser.parse_args()
+
+    if os.path.isfile(args.sequence):
+        args.sequence = open(args.sequence).read().rstrip()
+
+    return args
 
 
 # --------------------------------------------------
 def complement_and_reverse(seq: str) -> str:
     """Reverse the DNA sequence and get the complement, respecting the case"""
-
+   
+    # add in the lower case here too so you can match the case from the user
     complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
     transformed = ''.join(complement[base.upper()]
                           if base.isupper()
                           else complement[base.upper()].lower()
                           for base in reversed(seq))
+        
     return transformed
 
 # --------------------------------------------------
